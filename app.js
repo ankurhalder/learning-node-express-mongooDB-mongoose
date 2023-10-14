826857e42631894f85049b3596743a51df1ef8c0
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-14 21:03:21 +0530

    i am successfully updating my code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -8,11 +9,42 @@ const tours = JSON.parse(
 
 app.get(`/api/v1/tours`, (req, res) => {
   res.status(200).json({
-    status: 'success hehe boi',
+    status: 'success',
     data: {
       tours,
     },
   });
+});
+
+app.post(`/api/v1/tours`, (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign(
+    {
+      id: newId,
+    },
+    req.body
+  );
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        res.status(500).json({
+          status: 'error',
+          message: 'Failed to write data to file.',
+        });
+      } else {
+        res.status(201).json({
+          status: 'success',
+          data: {
+            tour: newTour,
+          },
+        });
+      }
+    }
+  );
 });
 
 const port = 3000;
