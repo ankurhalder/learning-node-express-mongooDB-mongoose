670d73aa840cc84db8c2f69b6766ebec75a824d4
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-14 18:13:03 +0530

    simple get request created

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello I am a server side created by Ankur Halder');
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}....`);
