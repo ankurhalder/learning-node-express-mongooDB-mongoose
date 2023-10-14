244c0d125dbbfd2a3b05ae011034617f19b0d44d
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-14 18:18:06 +0530

    so cool stuff

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 
+// my first get method
 app.get('/', (req, res) => {
   res.status(200).json({
     message: 'Hello I am a server side created by Ankur Halder',
     app: 'Natours',
   });
 });
-
+// my first post method
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint created by Ankur Halder...');
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}....`);
