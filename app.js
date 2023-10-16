e24093f32c63ae3818ad34efeb63ba3ce4e5a4dd
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-16 16:50:04 +0530

    refractored my code using routes

--- app.js
+++ app.js
@@ -89,12 +89,19 @@ const deleteTour = (req, res) => {
   });
 };
 
-app.get(`/api/v1/tours`, getAllTours);
-app.get(`/api/v1/tours/:id`, getTour);
-app.post(`/api/v1/tours ,`, createTour);
-app.patch(`/api/v1/tours/:id`, updateTour);
-app.delete(`/api/v1/tours/:id`, deleteTour);
+// app.get(`/api/v1/tours`, getAllTours);
+// app.get(`/api/v1/tours/:id`, getTour);
+// app.post(`/api/v1/tours ,`, createTour);
+// app.patch(`/api/v1/tours/:id`, updateTour);
+// app.delete(`/api/v1/tours/:id`, deleteTour);
 
+app.route(`/api/v1/tours`).get(getAllTours).post(createTour);
+
+app
+  .route(`/api/v1/tours/:id`)
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}....`);
