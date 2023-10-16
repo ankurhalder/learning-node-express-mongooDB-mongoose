25fd1febb2021b7b050b77323707ecedba410ea9
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-16 21:46:06 +0530

    learned how to use static server using express

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello By Ankur Halder from the middleware😊');
