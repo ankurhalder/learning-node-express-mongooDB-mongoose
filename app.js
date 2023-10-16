b5586f0e6ed2dfa712a233349ac185a2b3e4a628
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-16 20:55:28 +0530

    i love node.js

--- app.js
+++ app.js
@@ -24,8 +24,4 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// 4 Start Server
-const port = 3001;
-app.listen(port, () => {
-  console.log(`App running on port ${port}....`);
-});
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+const port = 3001;
+app.listen(port, () => {
+  console.log(`App running on port ${port}....`);
+});
