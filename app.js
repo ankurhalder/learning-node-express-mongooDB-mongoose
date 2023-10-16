ea8e335d69fc15c38349f545aed39ef9e0aea46f
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-16 20:12:21 +0530

    users router is still working

--- app.js
+++ app.js
@@ -107,34 +107,35 @@ const deleteTour = (req, res) => {
 };
 
 const getAllUsers = (req, res) => {
-  res.status().json({
+  res.status(501).json({
     status: 'error',
     message: 'This route is not yet defined',
   });
 };
+
 const getUser = (req, res) => {
-  res.status().json({
+  res.status(501).json({
     status: 'error',
     message: 'This route is not yet defined',
   });
 };
 
 const createUser = (req, res) => {
-  res.status().json({
+  res.status(501).json({
     status: 'error',
     message: 'This route is not yet defined',
   });
 };
 
 const updateUser = (req, res) => {
-  res.status().json({
+  res.status(501).json({
     status: 'error',
     message: 'This route is not yet defined',
   });
 };
 
 const deleteUser = (req, res) => {
-  res.status().json({
+  res.status(501).json({
     status: 'error',
     message: 'This route is not yet defined',
   });
