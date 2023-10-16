d539295b0982cab985911efafcf0292eab54a1af
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-16 19:40:08 +0530

    my first middleware added

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express();
 // we are using middleware to parse the body of the request
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('Hello By Ankur Halder from the middleware😊');
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
