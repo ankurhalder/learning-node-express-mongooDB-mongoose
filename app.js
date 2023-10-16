89b742bf282e54d9580e0a882267e700ec984b5a
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-16 20:26:19 +0530

    all routers are mounted

--- app.js
+++ app.js
@@ -141,21 +141,18 @@ const deleteUser = (req, res) => {
   });
 };
 
-// 3 Routes
-app.route(`/api/v1/tours`).get(getAllTours).post(createTour);
+// 3 Routes(Mounting)
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-app
-  .route(`/api/v1/tours/:id`)
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+tourRouter.route(`/`).get(getAllTours).post(createTour);
+tourRouter.route(`/:id`).get(getTour).patch(updateTour).delete(deleteTour);
 
-app.route(`/api/v1/users`).get(getAllUsers).post(createUser);
-app
-  .route(`/api/v1/users/:id`)
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+userRouter.route(`/`).get(getAllUsers).post(createUser);
+userRouter.route(`/:id`).get(getUser).patch(updateUser).delete(deleteUser);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 // 4 Start Server
 const port = 3001;
