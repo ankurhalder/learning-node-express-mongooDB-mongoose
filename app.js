80edc2e5dda0a99d52dd4d6fd9ca9482bdeac8e4
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-16 16:37:28 +0530

    delete method leared

--- app.js
+++ app.js
@@ -80,7 +80,18 @@ app.patch(`/api/v1/tours/:id`, (req, res) => {
     },
   });
 });
-
+app.delete(`/api/v1/tours/:id`, (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}....`);
