b1121f36c66ea2ae6d1eb38e0152dfad8c726507
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-18 22:30:05 +0530

    leared  new scheams

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,14 +6,54 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a name'],
     unique: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    //  trim will remove all the white spaces in the beginning and end of the string
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  //  images is an array of strings
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    //  select: false will hide the field from the output
+    select: false,
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('tour', tourSchema);
