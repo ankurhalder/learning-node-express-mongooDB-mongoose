e3c877f9caafdedc2ff094bbbb56decc4a6c43b9
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-18 17:56:12 +0530

    database connected successfully

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 /* eslint-disable no-console */
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({
   path: './config.env',
 });
+const DB =
+  'mongodb+srv://ankur:<PASSWORD>@natours.vr1povc.mongodb.net/?retryWrites=true&w=majority'.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD,
+  );
+
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
+
 const app = require('./app');
 
 console.log(app.get('env'));
-// console.log(process.env);
 const port = process.env.PORT || 8000;
-// const port = 3001;
 app.listen(port, () => {
   console.log(`App running on port ${port}....`);
 });
