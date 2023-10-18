fd919c576b30344bece9d49a35a978a93d126f8d
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-18 18:33:09 +0530

    creatyed my first schema

--- server.js
+++ server.js
@@ -1,19 +1,39 @@
 /* eslint-disable no-console */
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const app = require('./app');
 
 dotenv.config({
   path: './config.env',
 });
+
 const DB =
   'mongodb+srv://ankur:<PASSWORD>@natours.vr1povc.mongodb.net/?retryWrites=true&w=majority'.replace(
     '<PASSWORD>',
     process.env.DATABASE_PASSWORD,
   );
 
-mongoose.connect(DB).then(() => console.log('DB connection successful!'));
+mongoose
+  .connect(DB)
+  .then(() => console.log('DB connection successful!'))
+  .catch((err) => console.log(err));
 
-const app = require('./app');
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
 
 console.log(app.get('env'));
 const port = process.env.PORT || 8000;
