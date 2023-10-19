1ad4a8a70022783e9c92f4e8bcba90fb164f9c95
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-19 22:25:58 +0530

    in authorization bug fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,12 +2,18 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  // const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
   res.status(201).json({
     status: 'success',
     data: {
-      user: newUser
-    }
+      user: newUser,
+    },
   });
 });
