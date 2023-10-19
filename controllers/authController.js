320c852af1b35e89c4fff5a52a57562fdc1b654f
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-20 02:14:51 +0530

    sending email for reset by token

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const sendEmail = require('../utils/email');
 
 const signToken = (id) =>
   jwt.sign({ id: id }, process.env.JWT_SECRET, {
@@ -121,5 +122,32 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const resetToken = user.createPasswordResetToken();
   await user.save({ validateBeforeSave: false });
   // 3) Send it to user's email
+  const resetURL = `${req.protocol}://${req.get(
+    'host',
+  )}/api/v1/users/resetPassword/${resetToken}`;
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10min)',
+      message,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email!',
+    });
+  } catch (error) {
+    // console.log(error);
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+    // console.log(error);
+    return next(
+      new AppError('There was an error sending the email. Try again later!'),
+      500,
+    );
+  }
 });
 exports.resetPassword = catchAsync(async (req, res, next) => {});
---
+++ utils/email.js
@@ -0,0 +1,53 @@
+const nodemailer = require('nodemailer');
+
+// #gmail
+// const sendEmail = async (options) => {
+//   // 1) Create a transporter
+//   const transporter = nodemailer.createTransport({
+//     service: 'Gmail',
+//     auth: {
+//       user: process.env.EMAIL_USERNAME,
+//       pass: process.env.EMAIL_PASSWORD,
+//     },
+//   });
+
+//   // 2) Define the email options
+//   const mailOptions = {
+//     from: 'Jonas Schmedtmann <hello@jonas>',
+//     to: options.email,
+//     subject: options.subject,
+//     text: options.message,
+//     // html:
+//   };
+
+//   // 3) Actually send the email
+//   await transporter.sendMail(mailOptions);
+// };
+
+// #mailtrap
+
+const sendEmail = async (options) => {
+  // 1) Create a transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  // 2) Define the email options
+  const mailOptions = {
+    from: 'Jonas Schmedtmann <hello@jonas.io>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html:
+  };
+
+  // 3) Actually send the email
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
