3e07b7667fc81e1b7f1140603081df9f2e4eba7b
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-19 18:39:27 +0530

    user sugnup feature added

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(() => console.log('DB connection successful!'));
 
