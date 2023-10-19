68c7c5c964e09c6b5d8b468d3b36224db911ba51
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-20 01:40:00 +0530

    User roll and permission mechanisn added

--- controllers/authController.js
+++ controllers/authController.js
@@ -93,7 +93,20 @@ exports.protect = catchAsync(async (req, res, next) => {
       new AppError('User recently changed password! Please log in again.', 401),
     );
   }
-  // grant access to protected route
+  //! grant access to protected route
   req.user = currentUser;
   next();
 });
+
+exports.restrictTo =
+  (...roles) =>
+  (req, res, next) => {
+    // roles ['admin', 'lead-guide'] role='user'
+    // console.log(req.user.role);
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action.', 403),
+      );
+    }
+    next();
+  };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -22,6 +22,11 @@ router
   .route('/:id')
   .get(tourController.getTour)
   .patch(tourController.updateTour)
-  .delete(tourController.deleteTour);
+  //! Authenticated users only
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin' || 'lead-guide'),
+    tourController.deleteTour,
+  );
 
 module.exports = router;
