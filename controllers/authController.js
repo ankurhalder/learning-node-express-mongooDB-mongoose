14f6f8042b30d6f7c6559c2907b2b95a9acd3fd5
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-20 01:23:07 +0530

    minor bug fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -77,8 +77,8 @@ exports.protect = catchAsync(async (req, res, next) => {
   // console.log(decoded);
 
   // 3) Check if user still exists
-  const freshUser = await User.findById(decoded.id);
-  if (!freshUser) {
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
     return next(
       new AppError(
         'The user belonging to this token does no longer exist.',
@@ -87,13 +87,13 @@ exports.protect = catchAsync(async (req, res, next) => {
     );
   }
   // 4) Check if user changed password after the token was issued
-  freshUser.changedPasswordAfter(decoded.iat);
-  if (freshUser.changedPasswordAfter(decoded.iat)) {
+  currentUser.changedPasswordAfter(decoded.iat);
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
     return next(
       new AppError('User recently changed password! Please log in again.', 401),
     );
   }
   // grant access to protected route
-  req.user = freshUser;
+  req.user = currentUser;
   next();
 });
