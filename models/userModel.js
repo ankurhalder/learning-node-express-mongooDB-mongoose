b3ed277bfdd5114dd22272c4f9c6ebebb4e3233f
Ankur Halder <ankur.halder12345@gmail.com>
2023-10-20 01:52:50 +0530

    implementing reset password part -1

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 /* eslint-disable node/no-unsupported-features/es-syntax */
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -81,6 +82,18 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   // False means NOT changed
   return false;
 };
+// # this will help us to create a token for the user to reset the password
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+  console.log({ resetToken }, this.passwordResetToken);
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+  return resetToken;
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
